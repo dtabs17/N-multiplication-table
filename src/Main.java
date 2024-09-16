87c729ff703d2895cc9b566119a5d8e2f351e6cb
dtabs17 <davidadebanwo16@gmail.com>
2024-09-16 14:48:08 +0100

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a positive number: ");
+        int num = sc.nextInt();
+        for (int i = 1; i <= num; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j*i + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
